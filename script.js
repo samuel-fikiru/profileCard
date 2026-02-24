5c07d98d85c36b08496cd2c37e19b5c0453fd1f2
Samuel <smfikiru@gmail.com>
2026-02-24 14:27:02 +0300

    profile card complete

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ updateFollowerCount();
 
 const followButton =document.querySelector(".js-folllow-button");
 followButton.addEventListener('click', ()=>{
-    console.log("followed!")
     followerCount++;
     updateFollowerCount();
 })
